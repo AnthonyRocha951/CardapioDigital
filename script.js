4e887d4a4896fc737887e66fb19e902859b915c5
AnthonyRocha <rochaanthony159@gmail.com>
2024-10-29 22:13:07 -0300

    att

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const cartCounter = document.getElementById("cart-count")
 const addressInput = document.getElementById("address")
 const addressWarn = document.getElementById("address-warn")
 
+let cart = [];
+
 // abrir o modal do carrinho
 cartBtn.addEventListener("click", function(){
     cartModal.style.display = "flex"
@@ -26,3 +28,68 @@ cartModal.addEventListener("click", function(event){
 closeModalBtn.addEventListener("click", function(){
     cartModal.style.display = "none"
 })
+
+menu.addEventListener("click", function(event){
+    // console.log(event.target) 
+
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if(parentButton) {
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        addToCart(name, price)
+
+    }
+
+})
+
+// funçao para adicionar no carrinho
+function addToCart(name, price) {
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem) {
+        // se o item ja existe, aumenta apenas a quantidade + 1
+        existingItem.quantity += 1;
+
+    }else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        })
+    }
+    
+    updateCartModal()
+
+}
+
+//atualiza carrinho
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div")
+
+        cartItemElement.innerHTML = `
+          <div>
+            <div>
+              <p>${item.name}</p>
+              <p>${item.quantity}</p>
+              <p>R$ ${item.price}</p>
+            </div>
+
+            <div>
+              <button>
+                Remover
+              </button>
+            </div>
+          </div>
+        `
+
+        cartItemsContainer.appendChild(cartItemElement)
+
+    })
+
+}
